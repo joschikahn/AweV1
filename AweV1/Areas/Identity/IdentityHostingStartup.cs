c9b1d12ac120197eb649246e033fe97c213ebba3
Josua Homberg <josua.homberg@online.de>
2020-07-01 17:27:01 +0200

    Add project files.

---
+++ AweV1/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AweV1.Areas.Identity.Data;
+using AweV1.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AweV1.Areas.Identity.IdentityHostingStartup))]
+namespace AweV1.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+
+        }
+    }
+}
